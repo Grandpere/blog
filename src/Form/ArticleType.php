a08afc23afa30dd9361673baeb0e5e5c9253af5a
Grandpere <lorenzo.marozzo@gmail.com>
2020-03-30 14:04:54 +0200

    forgotten formTypes commit

---
+++ src/Form/ArticleType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Article;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ArticleType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title')
+            ->add('content')
+            ->add('coverImage')
+            ->add('isActive')
+            //->add('tags')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Article::class,
+            'csrf_protection' => false
+        ]);
+    }
+}
