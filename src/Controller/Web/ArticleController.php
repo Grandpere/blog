750f9d3ff9d64d207e4246c81fe045760bce1cf6
Grandpere <lorenzo.marozzo@gmail.com>
2020-04-01 14:12:33 +0200

    update Web Article & Tag controller + templates

--- src/Controller/Web/ArticleController.php
+++ src/Controller/Web/ArticleController.php
@@ -2,21 +2,37 @@
 
 namespace App\Controller\Web;
 
+use App\Entity\Article;
 use App\Repository\ArticleRepository;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+/**
+ * @Route("/articles", name="web_articles_")
+ */
 class ArticleController extends AbstractController
 {
     /**
-     * @Route("/web/article", name="web_article")
+     * @Route("/", name="index", methods={"GET"})
      */
     public function index(ArticleRepository $articleRepository)
     {
         $articles = $articleRepository->findAll();
         return $this->render('web/article/index.html.twig', [
-            'controller_name' => 'ArticleController',
             'articles' => $articles,
+        ]);
+    }
+
+    /**
+     * @Route("/{slug}", name="show", methods={"GET"}, requirements={"slug"="[a-zA-Z0-9-]+"})
+     */
+    public function show(Article $article = null)
+    {
+        if(!$article) {
+            throw $this->createNotFoundException('Article introuvable');
+        }
+        return $this->render('web/article/show.html.twig', [
+            'article' => $article,
         ]);
     }
 }
--- src/Controller/Web/TagController.php
+++ src/Controller/Web/TagController.php
@@ -2,18 +2,37 @@
 
 namespace App\Controller\Web;
 
+use App\Entity\Tag;
+use App\Repository\TagRepository;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+/**
+ * @Route("/tags", name="web_tags_")
+ */
 class TagController extends AbstractController
 {
     /**
-     * @Route("/web/tag", name="web_tag")
+     * @Route("/", name="index", methods={"GET"})
      */
-    public function index()
+    public function index(TagRepository $tagRepository)
     {
+        $tags = $tagRepository->findAll();
         return $this->render('web/tag/index.html.twig', [
-            'controller_name' => 'TagController',
+            'tags' => $tags,
+        ]);
+    }
+
+    /**
+     * @Route("/{slug}", name="show", methods={"GET"}, requirements={"slug"="[a-zA-Z0-9-]+"})
+     */
+    public function show(Tag $tag = null)
+    {
+        if(!$tag) {
+            throw $this->createNotFoundException('Tag introuvable');
+        }
+        return $this->render('web/tag/show.html.twig', [
+            'tag' => $tag,
         ]);
     }
 }
