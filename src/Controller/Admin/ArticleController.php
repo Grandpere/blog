e8af40f7963aa434f9a4ad029441b94d4f47d1c0
Grandpere <lorenzo.marozzo@gmail.com>
2020-04-13 11:49:47 +0200

    Update AdminArticleController + templates

--- src/Controller/Admin/ArticleController.php
+++ src/Controller/Admin/ArticleController.php
@@ -3,54 +3,100 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Article;
-use App\Form\ArticleType;
+use App\Form\Article1Type;
+use App\Repository\ArticleRepository;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * Class ArticleController
- * @package App\Controller\Admin
  * @Route("/admin/article", name="admin_article_")
  */
 class ArticleController extends AbstractController
 {
     /**
-     * @Route("/", name="index")
+     * @Route("/", name="index", methods={"GET"})
      */
-    public function index()
+    public function index(ArticleRepository $articleRepository): Response
     {
         return $this->render('admin/article/index.html.twig', [
-            'controller_name' => 'ArticleController',
+            'articles' => $articleRepository->findAll(),
         ]);
     }
 
     /**
-     * @Route("/create", name="new")
+     * @Route("/new", name="new", methods={"GET","POST"})
      * @IsGranted("ROLE_ADMIN_ARTICLE")
      */
-    public function new()
+    public function new(Request $request): Response
     {
-        $form = $this->createForm(ArticleType::class);
+        $article = new Article();
+        $form = $this->createForm(Article1Type::class, $article);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($article);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('admin_article_index');
+        }
 
         return $this->render('admin/article/new.html.twig', [
-            'articleForm' => $form->createView(),
+            'article' => $article,
+            'form' => $form->createView(),
         ]);
     }
 
     /**
-     * @Route("/{id}/edit", name="edit")
+     * @Route("/{id}", name="show", methods={"GET"})
+     */
+    public function show(Article $article): Response
+    {
+        return $this->render('admin/article/show.html.twig', [
+            'article' => $article,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="edit", methods={"GET","POST"})
      * @IsGranted("MANAGE", subject="article")
      */
-    public function edit(Article $article)
+    public function edit(Request $request, Article $article): Response
     {
         // if Article $article are argument, IsGranted annotation with subject don't works
         // this only works because we used the feature that automatically queries for the Article object and passes it as an argument
         // Without this use : $this->denyAccessUnlessGranted('MANAGE', $article);
         // $this->denyAccessUnlessGranted('MANAGE', $article);
 
-        dd($article);
+        $form = $this->createForm(Article1Type::class, $article);
+        $form->handleRequest($request);
 
-        //return $this->json(['ok']);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('admin_article_index');
+        }
+
+        return $this->render('admin/article/edit.html.twig', [
+            'article' => $article,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="delete", methods={"DELETE"})
+     */
+    public function delete(Request $request, Article $article): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$article->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($article);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('admin_article_index');
     }
 }
