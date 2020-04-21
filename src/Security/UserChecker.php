e1342bcadc71ce01a4964830dd35ea51042194b9
Grandpere <lorenzo.marozzo@gmail.com>
2020-04-21 19:19:44 +0200

    Update account template & css

---
+++ src/Security/UserChecker.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Security;
+
+use App\Entity\User;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\UserCheckerInterface;
+use Symfony\Component\Security\Core\Exception\AccountExpiredException;
+use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationException;
+
+class UserChecker implements UserCheckerInterface
+{
+    public function checkPreAuth(UserInterface $user)
+    {
+        if (!$user instanceof User) {
+            return;
+        }
+
+        if (!$user->getIsActive()) {
+            throw new \Exception('Account not activated');
+        }
+    }
+
+    public function checkPostAuth(UserInterface $user)
+    {
+        if (!$user instanceof User) {
+            return;
+        }
+
+        // user account is expired, the user may be notified
+        // if ($user->isExpired()) {
+        //     throw new AccountExpiredException('...');
+        // }
+    }
+}
