5d0d70b80461f6ace3f8a73649b31213884a0aa5
Grandpere <lorenzo.marozzo@gmail.com>
2020-04-28 14:45:35 +0200

    add welcome flash message after login

--- src/EventListener/LoginListener.php
+++ src/EventListener/LoginListener.php
@@ -4,19 +4,29 @@ namespace App\EventListener;
 
 use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\Security\Http\Event\InteractiveLoginEvent;
 
 class LoginListener
 {
+    /**
+     * @var EntityManagerInterface
+     */
     private $em;
+    /**
+     * @var Session
+     */
+    private $session;
 
-    public function __construct(EntityManagerInterface $em)
+    public function __construct(EntityManagerInterface $em, Session $session)
     {
         $this->em = $em;
+        $this->session = $session;
     }
 
     public function onSecurityInteractiveLogin(InteractiveLoginEvent $event)
     {
+        /** @var User $user */
         // Get the User entity.
         $user = $event->getAuthenticationToken()->getUser();
 
@@ -26,7 +36,8 @@ class LoginListener
         // Persist the data to database.
         $this->em->persist($user);
         $this->em->flush();
-    }
 
-    // TODO: add Welcome Message + last Login in flash messages
+        // welcome flash message to user
+        $this->session->getFlashBag()->add('success','Hello '.$user->getUsername());
+    }
 }
