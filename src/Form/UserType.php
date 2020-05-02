0f3f7f81ff437715ac0bdcec035e59b3e4cbe836
Grandpere <lorenzo.marozzo@gmail.com>
2020-05-02 12:04:19 +0200

    CRUD on User entity added for testing impersonate user

---
+++ src/Form/UserType.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('email')
+            ->add('roles')
+            ->add('password')
+            ->add('firstname')
+            ->add('lastname')
+            ->add('avatar')
+            ->add('isActive')
+            ->add('lastLogin')
+            ->add('accountValidationToken')
+            ->add('validationTokenCreatedAt')
+            ->add('resetPasswordToken')
+            ->add('passwordTokenCreatedAt')
+            ->add('agreedTermsAt')
+            ->add('createdAt')
+            ->add('updatedAt')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
