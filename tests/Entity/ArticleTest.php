3c13355a3dd03eecd51e4a1be4dd5154963dfb77
Grandpere <lorenzo.marozzo@gmail.com>
2020-05-06 18:07:55 +0200

    Improve Article entity tests

--- tests/Entity/ArticleTest.php
+++ tests/Entity/ArticleTest.php
@@ -3,21 +3,28 @@
 namespace App\Tests\Entity;
 
 use App\Entity\Article;
+use App\Entity\Comment;
+use App\Entity\Tag;
 use App\Entity\User;
 use Doctrine\Common\Collections\ArrayCollection;
 use PHPUnit\Framework\TestCase;
 
 class ArticleTest extends TestCase
 {
+    private $article;
+
+    public function setUp()
+    {
+        $this->article = new Article();
+    }
+
     public function testArticleAfterConstructor()
     {
-        $article = new Article();
-
-        $this->assertInstanceOf(ArrayCollection::class, $article->getTags());
-        $this->assertEmpty($article->getTags());
-        $this->assertInstanceOf(\DateTime::class, $article->getCreatedAt());
+        $this->assertInstanceOf(ArrayCollection::class, $this->article->getTags());
+        $this->assertEmpty($this->article->getTags());
+        $this->assertInstanceOf(\DateTime::class, $this->article->getCreatedAt());
         $this->assertIsBool(false);
-        $this->assertInstanceOf(ArrayCollection::class, $article->getComments());
+        $this->assertInstanceOf(ArrayCollection::class, $this->article->getComments());
     }
 
     /**
@@ -26,16 +33,14 @@ class ArticleTest extends TestCase
      */
     public function testSetStringColumnWithCorrectType($columnName)
     {
-        $article = new Article();
+        $this->article->{'set'.$columnName}('Test '.$columnName);
+        $this->assertEquals('Test '.$columnName, $this->article->{'get'.$columnName}());
 
-        $article->{'set'.$columnName}('Test '.$columnName);
-        $this->assertEquals('Test '.$columnName, $article->{'get'.$columnName}());
+        $this->article->{'set'.$columnName}('');
+        $this->assertEquals('', $this->article->{'get'.$columnName}());
 
-        $article->{'set'.$columnName}('');
-        $this->assertEquals('', $article->{'get'.$columnName}());
-
-        $article->{'set'.$columnName}('Test '.$columnName.$i=1);
-        $this->assertEquals('Test '.$columnName.$i, $article->{'get'.$columnName}());
+        $this->article->{'set'.$columnName}('Test '.$columnName.$i=1);
+        $this->assertEquals('Test '.$columnName.$i, $this->article->{'get'.$columnName}());
     }
 
     public function getStringColumnName()
@@ -56,10 +61,8 @@ class ArticleTest extends TestCase
      */
     public function testSetStringColumnWithIncorrectType($columnName, $columnData)
     {
-        $article = new Article();
-
         $this->expectException('\TypeError');
-        $article->{'set'.$columnName}($columnData);
+        $this->article->{'set'.$columnName}($columnData);
     }
 
     public function getStringColumnNameAndInvalidData()
@@ -79,32 +82,26 @@ class ArticleTest extends TestCase
 
     public function testSetUpdatedAtWithCorrectType()
     {
-        $article = new Article();
+        $this->assertNull($this->article->getUpdatedAt());
 
-        $this->assertNull($article->getUpdatedAt());
-
-        $article->setUpdatedAt(new \DateTime('2020-05-05'));
-        $this->assertEquals('2020-05-05', $article->getUpdatedAt()->format('Y-m-d'));
+        $this->article->setUpdatedAt(new \DateTime('2020-05-05'));
+        $this->assertEquals('2020-05-05', $this->article->getUpdatedAt()->format('Y-m-d'));
     }
 
     public function testSetUpdatedAtWithIncorrectType()
     {
-        $article = new Article();
-
         $this->expectException('\TypeError');
-        $article->setUpdatedAt('2020-05-05');
+        $this->article->setUpdatedAt('2020-05-05');
     }
 
     public function testSetIsActiveWithCorrectType()
     {
-        $article = new Article();
-
         $this->assertIsBool(false);
-        $this->assertEquals(false, $article->getIsActive());
+        $this->assertEquals(false, $this->article->getIsActive());
 
-        $article->setIsActive(true);
+        $this->article->setIsActive(true);
         $this->assertIsBool(true);
-        $this->assertEquals(true, $article->getIsActive());
+        $this->assertEquals(true, $this->article->getIsActive());
     }
 
     public function testSetAuthor()
@@ -112,30 +109,84 @@ class ArticleTest extends TestCase
         $user = new User();
         $user->setEmail('test@test.com');
 
-        $article = new Article();
-        $article->setAuthor($user);
+        $this->article->setAuthor($user);
 
-        $this->assertInstanceOf(User::class, $article->getAuthor());
-        $this->assertEquals('test@test.com', $article->getAuthor()->getEmail());
+        $this->assertInstanceOf(User::class, $this->article->getAuthor());
+        $this->assertEquals('test@test.com', $this->article->getAuthor()->getEmail());
     }
 
     public function testSetAuthorWithIncorrectType()
     {
-        $article = new Article();
-
         $this->expectException('\TypeError');
-        $article->setAuthor('user');
+        $this->article->setAuthor('user');
     }
 
     public function testTags()
     {
-        $this->markTestIncomplete('TODO');
+        $tagOne = new Tag();
+        $tagOne->setTitle('tag1');
+        $tagTwo = new Tag();
+        $tagTwo->setTitle('tag2');
+        $this->article->addTag($tagOne);
+        $this->article->addTag($tagTwo);
+        $tags = $this->article->getTags();
+
+        $this->assertInstanceOf(ArrayCollection::class, $tags);
+        $this->assertInstanceOf(Tag::class, $tags[0]);
+        $this->assertSame('tag1', $tags[0]->getTitle());
+
+        $this->assertCount(2, $tags);
+        $this->article->removeTag($tagTwo);
+        $this->assertCount(1, $tags);
     }
 
     public function testComments()
     {
-        $this->markTestIncomplete('TODO');
-    f}
+        $commentOne = new Comment();
+        $commentOne->setContent('comment test 1');
+        $commentTwo = new Comment();
+        $commentTwo->setContent('comment test 2');
+        $this->article->addComment($commentOne);
+        $this->article->addComment($commentTwo);
+        $comments = $this->article->getComments();
+
+        $this->assertInstanceOf(ArrayCollection::class, $comments);
+        $this->assertInstanceOf(Comment::class, $comments[0]);
+        $this->assertSame('comment test 1', $comments[0]->getContent());
+
+        $this->assertCount(2, $comments);
+        $this->article->removeComment($commentTwo);
+        $this->assertCount(1, $comments);
+    }
+
+    /**
+     * @param $dataColumn
+     * @param $action
+     * @dataProvider getInvalidTagAndCommentType
+     */
+    public function testCommentsOrTagsWithIncorrectType($columnData, $columnName, $action)
+    {
+        $this->expectException('\TypeError');
+
+        switch ($action) {
+            case 'add' :
+                $this->article->{'add'.$columnName}($columnData);
+                break;
+            case 'remove' :
+                $this->article->{'remove'.$columnName}($columnData);
+                break;
+        }
+    }
+
+    public function getInvalidTagAndCommentType()
+    {
+        return [
+            ['test', 'Tag', 'add'],
+            ['test', 'Tag', 'remove'],
+            ['test', 'Comment', 'add'],
+            ['test', 'Comment', 'remove'],
+        ];
+    }
 
     public function testImageFile()
     {
