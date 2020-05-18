72393fbd99e5656e0c429b42c8f2930dc4e701dc
Grandpere <lorenzo.marozzo@gmail.com>
2020-05-18 15:37:45 +0200

    ContactController tests creation

---
+++ tests/Controller/Web/ContactControllerTest.php
@@ -0,0 +1,31 @@
+<?php
+
+
+namespace App\Tests\Controller\Web;
+
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Response;
+
+class ContactControllerTest extends WebTestCase
+{
+    private $client = null;
+
+    public function setUp(): void
+    {
+        $this->client = static::createClient();
+    }
+
+    public function tearDown(): void
+    {
+        $this->client = null;
+    }
+
+    public function testGetContact()
+    {
+        // TODO : tests env variable for prevent error 500
+        $crawler = $this->client->request('GET', '/contact');
+        dump($this->client);
+        $this->assertEquals(Response::HTTP_OK, $this->client->getResponse()->getStatusCode());
+    }
+}
