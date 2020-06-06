e1350d210a3a2411b12f9a43e14787d53389bdae
Grandpere <lorenzo.marozzo@gmail.com>
2020-06-06 13:42:04 +0200

    Webpack Encore installation & configuration + begginning refactor assets with tags autocomplete in article creation/edition

---
+++ assets/js/article_tag-autocomplete.js
@@ -0,0 +1,35 @@
+import $ from 'jquery'; // works without jquery import because autoProvidejQuery() is enabled?
+import 'bootstrap';     // works without bootstrap import because autoProvidejQuery() is enabled?
+import 'bootstrap-tagsinput';
+import Bloodhound from 'typeahead.js';
+
+import 'bootstrap-tagsinput/dist/bootstrap-tagsinput.css';
+import 'bootstrap-tagsinput/dist/bootstrap-tagsinput-typeahead.css';
+import  '../css/components/_bootstrap_tag-input.scss';
+
+
+$(document).ready(function() {
+    const url = $('.tag-input').data('autocomplete-url');
+
+    var tags = new Bloodhound({
+        prefetch: url,
+        datumTokenizer: Bloodhound.tokenizers.obj.whitespace('title'),
+        queryTokenizer: Bloodhound.tokenizers.whitespace,
+    });
+
+    $('.tag-input').tagsinput({
+        tagClass: 'badge badge-secondary', // because label label-info not supported in bootstrap4
+        confirmKeys: [13, 44],
+        trimValue: true,
+        typeaheadjs: [{
+            highlights: true
+        }, {
+            name: 'tags',
+            displayKey: 'title',
+            valueKey: 'title',
+            source: tags
+        }]
+    });
+
+    $('.bootstrap-tagsinput').addClass('form-control bti'); // add form-control class for bootstrap4 class & custom bti class for height:auto
+});
