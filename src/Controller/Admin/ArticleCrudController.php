63ee0aee56cb0ae527e671bd44c1216636066a9a
Grandpere <lorenzo.marozzo@gmail.com>
2020-07-13 00:10:02 +0200

    create Admin Dashboard & beginning of custom menu and fields for article

---
+++ src/Controller/Admin/ArticleCrudController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Article;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\SlugField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+
+class ArticleCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Article::class;
+    }
+
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            IdField::new('id')->hideOnForm(),
+            TextField::new('title'),
+            SlugField::new('slug')->setTargetFieldName('title')->hideOnForm(),
+            TextEditorField::new('content'),
+            TextareaField::new('excerpt'),
+            ImageField::new('coverImage'),
+            DateTimeField::new('createdAt')->hideOnForm(),
+            DateTimeField::new('updatedAt')->hideOnForm(),
+            BooleanField::new('isactive', 'Active'),
+            BooleanField::new('report'),
+            BooleanField::new('moderate'),
+            AssociationField::new('author')->hideOnForm()
+        ];
+    }
+}
---
+++ src/Controller/Admin/DashboardController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Article;
+use App\Entity\Comment;
+use App\Entity\Like;
+use App\Entity\Tag;
+use App\Entity\User;
+use App\Entity\View;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Config\UserMenu;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class DashboardController extends AbstractDashboardController
+{
+    /**
+     * @Route("/admin", name="admin")
+     */
+    public function index(): Response
+    {
+        return parent::index();
+    }
+
+    public function configureDashboard(): Dashboard
+    {
+        return Dashboard::new()
+            ->setTitle('Blog Api Admin Interface');
+    }
+
+    public function configureMenuItems(): iterable
+    {
+        yield MenuItem::linktoDashboard('Dashboard', 'fa fa-home');
+        // yield MenuItem::linkToCrud('The Label', 'icon class', EntityClass::class);
+
+        yield MenuItem::linkToCrud('Articles','fas fa-newspaper', Article::class);
+        yield MenuItem::linkToCrud('Tags','fas fa-tag', Tag::class);
+        yield MenuItem::linkToCrud('Comments','fas fa-comment', Comment::class);
+        yield MenuItem::linkToCrud('Likes','fas fa-thumbs-up', Like::class);
+        yield MenuItem::linkToCrud('Views','fas fa-eye', View::class);
+        yield MenuItem::linkToCrud('Users','fas fa-user', User::class);
+    }
+
+    public function configureUserMenu(UserInterface $user): UserMenu
+    {
+        return parent::configureUserMenu($user)
+            ->setAvatarUrl($user->getAvatar());
+    }
+
+}
