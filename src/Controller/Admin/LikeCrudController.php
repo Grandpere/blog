f102f978a1b9a9e712167e42ebf1535c067ed6be
Grandpere <lorenzo.marozzo@gmail.com>
2020-07-13 23:58:33 +0200

    customize likeCrudController

--- src/Controller/Admin/LikeCrudController.php
+++ src/Controller/Admin/LikeCrudController.php
@@ -3,7 +3,13 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Like;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 
 class LikeCrudController extends AbstractCrudController
 {
@@ -12,14 +18,20 @@ class LikeCrudController extends AbstractCrudController
         return Like::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
         return [
             IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            DateTimeField::new('likedAt'),
+            AssociationField::new('article')->hideOnForm(),
+            AssociationField::new('user')->hideOnForm(),
         ];
     }
-    */
+
+    public function configureActions(Actions $actions): Actions
+    {
+        return parent::configureActions($actions)
+            ->disable(Action::NEW, Action::EDIT, Action::DELETE)
+        ;
+    }
 }
