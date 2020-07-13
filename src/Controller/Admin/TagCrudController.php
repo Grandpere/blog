7433a13c131f5e8921410a695c1b06449b8e619d
Grandpere <lorenzo.marozzo@gmail.com>
2020-07-13 23:30:29 +0200

    customize tagCrudController

--- src/Controller/Admin/TagCrudController.php
+++ src/Controller/Admin/TagCrudController.php
@@ -4,6 +4,12 @@ namespace App\Controller\Admin;
 
 use App\Entity\Tag;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\SlugField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class TagCrudController extends AbstractCrudController
 {
@@ -12,14 +18,16 @@ class TagCrudController extends AbstractCrudController
         return Tag::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
+            IdField::new('id')->hideOnForm(),
             TextField::new('title'),
-            TextEditorField::new('description'),
+            SlugField::new('slug')->setTargetFieldName('title')->hideOnForm(),
+            DateTimeField::new('createdAt')->hideOnForm(),
+            DateTimeField::new('updatedAt')->hideOnForm(),
+            BooleanField::new('isActive', 'Active'),
+            AssociationField::new('articles')->hideOnForm()
         ];
     }
-    */
 }
